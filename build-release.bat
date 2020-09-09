5c6479d33317f0f24164862e7461c30ff21b33ac
Jovan Ivanovic <jovanivanovic@einutech.com>
2020-09-09 14:02:14 +0200

    Updated build scripts

---
+++ build-release.bat
@@ -0,0 +1,15 @@
+@echo off
+
+CD .\deps\altv-capi\
+if not exist .\tools\altv-capi-gen.exe call .\tools\get-gen.bat
+if not exist cpp-sdk call .\tools\get-cppsdk.bat
+
+CD .\capi\
+call .\gen-client.bat
+
+cmake -B BUILD --target altv-capi-client-static .
+cmake --build BUILD --target altv-capi-client-static
+
+CD ..\..\..\
+cmake -B BUILD
+cmake --build BUILD --config Release
