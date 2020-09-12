150205cf3d1d52222914466cdd33d825b1feaacd
Hazard <hazard9909@gmail.com>
2020-09-12 04:48:44 +0200

    Different exported function

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,11 @@
 #include "wasmtime.h"
 #include "util.hpp"
 
-CAPI_EXPORT alt::IScriptRuntime* altMain(alt::ICore* core)
+CAPI_EXPORT alt::IScriptRuntime* CreateWasmRuntime(alt::ICore* core)
 {
     alt::ICore::SetInstance(core);
 
-    // util::logi("[WASM] Initializing runtime...");
-    core->LogError("ok");
+    util::logi("[WASM] Initializing runtime...");
 
     auto runtime = new WasmRuntime();
     if(!runtime->Init())
