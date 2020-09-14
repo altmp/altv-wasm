5c156a405eaa554fa131b9888524490fb08ae3fc
Jovan Ivanovic <jovanivanovic@einutech.com>
2020-09-15 00:23:19 +0200

    WASM Pointer class

--- src/WasmPtr.hpp
+++ src/WasmPtr.hpp
@@ -1,8 +1,19 @@
-//
-// Created by Jovan Ivanovic on 9/14/2020.
-//
+#pragma once
 
-#ifndef ALTV_WASM_WASMPTR_HPP
-#define ALTV_WASM_WASMPTR_HPP
+#include <wasmtime.h>
+#include "Utilities.hpp"
 
-#endif //ALTV_WASM_WASMPTR_HPP
+template <typename T>
+class WasmPtr
+{
+public:
+    i32 offset = 0;
+
+    WasmPtr(const wasm_val_t& val) : offset(val.of.i32) {}
+    WasmPtr(i32 val) : offset(val) {}
+
+    T* get(wasm_memory_t* memory)
+    {
+        return (T*)(wasm_memory_data(memory) + offset);
+    }
+};
