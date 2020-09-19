b18b5d149957155021c84b895715f2505fb423b6
Jovan Ivanovic <jovanivanovic@einutech.com>
2020-09-19 23:35:55 +0200

    Test

--- src/WasmImport.cpp
+++ src/WasmImport.cpp
@@ -1,5 +1,4 @@
 #include "WasmImport.hpp"
-#include "Utilities.hpp"
 
 WasmImport::WasmImport(wasm_store_t *store, const std::string &module, const std::string &name, void *env, wasm_func_callback_with_env_t func, const std::initializer_list<wasm_valkind_enum> &params, const std::initializer_list<wasm_valkind_enum> &results)
     : func(func), module(module), name(name), env(env)
--- src/WasmRuntime.hpp
+++ src/WasmRuntime.hpp
@@ -22,8 +22,6 @@ public:
     WasmRuntime();
     bool Init();
 
-    wasm_func_t* CreateImportFunction(const std::string& name, wasm_func_callback_with_env_t func, void* env, const std::initializer_list<wasm_valkind_enum>& params, const std::initializer_list<wasm_valkind_enum>& results);
-
     virtual bool RequiresMain() const override;
 
     // Create resource
